803f5b2fe9beda25d18a8c2d8699bff0be2cca62
Lorenzo Setale <koalalorenzo@gmail.com>
2013-02-14 19:38:07 +0100

    Implemented __init__.py with new objects

--- digitalocean/__init__.py
+++ digitalocean/__init__.py
@@ -1,1 +1,14 @@
+
+"""digitalocean API to manage droplets"""
+
+__version__ = "0.1"
+__author__ = "Lorenzo Setale ( http://who.is.koalalorenzo.com/? )"
+__author_email__ = "koalalorenzo@gmail.com"
+__license__ = "See: http://creativecommons.org/licenses/by-nd/3.0/ "
+__copyright__ = "Copyright (c) 2012, 2013, 2014 Lorenzo Setale"
+
+from digitalocean.Manager import Manager
 from digitalocean.Droplet import Droplet
+from digitalocean.Region import Region
+from digitalocean.Size import Size
+from digitalocean.Image import Image

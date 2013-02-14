f85a078dbb6f70820cc667d9f4f7c87a53d6c7ee
Lorenzo Setale <koalalorenzo@gmail.com>
2013-02-14 19:37:12 +0100

    Created Image Object

---
+++ digitalocean/Image.py
@@ -0,0 +1,10 @@
+import requests
+
+class Image(object):
+    def __init__(self, client_id="", api_key=""):
+        self.client_id = client_id
+        self.api_key = api_key
+        
+        self.name = None
+        self.id = None
+        self.distribution = None

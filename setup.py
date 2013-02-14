199221c40f0c3c59ce5e4ef3e34703c62028f4ff
Lorenzo Setale <koalalorenzo@gmail.com>
2013-02-14 21:36:19 +0100

    Changed Description

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@
 
 from distutils.core import setup
 
+with open('README.md') as file:
+    long_description = file.read()
+
 setup(name='python-digitalocean',
-      version='0.2',
+      version='0.2.1',
       description='digitalocean.com API to manage Droplets and Images',
       author='Lorenzo Setale ( http://who.is.koalalorenzo.com/? )',
       author_email='koalalorenzo@gmail.com',
       url='https://github.com/koalalorenzo/python-digitalocean',
       packages=['digitalocean'],
+      long_description=long_description
      )

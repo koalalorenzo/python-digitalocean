b86e0a2d28cfa21bdb2acc089df70fb5a02b30e1
Lorenzo Setale <koalalorenzo@gmail.com>
2013-02-14 21:09:56 +0100

    Created setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='digitalocean',
+      version='0.1',
+      description='digitalocean API to manage droplets',
+      author='Lorenzo Setale ( http://who.is.koalalorenzo.com/? )',
+      author_email='koalalorenzo@gmail.com',
+      packages=['digitalocean'],
+     )

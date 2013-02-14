85251239baaf31c6e7d6b65e73740fb8165dc6fa
Lorenzo Setale <koalalorenzo@gmail.com>
2013-02-14 21:25:47 +0100

    Upgraded to 0.2 version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='python-digitalocean',
-      version='0.1',
+      version='0.2',
       description='digitalocean.com API to manage Droplets and Images',
       author='Lorenzo Setale ( http://who.is.koalalorenzo.com/? )',
       author_email='koalalorenzo@gmail.com',

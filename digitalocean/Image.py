298b8c579504c883466a49c2023f8c64660571f2
Lorenzo Setale <koalalorenzo@gmail.com>
2013-08-15 16:32:18 +0200

    Added Image function to handle images.

--- digitalocean/Image.py
+++ digitalocean/Image.py
@@ -8,3 +8,26 @@ class Image(object):
         self.name = None
         self.id = None
         self.distribution = None
+
+    def __call_api(self, path, params=dict()):
+        payload = {'client_id': self.client_id, 'api_key': self.api_key}
+        payload.update(params)
+        r = requests.get("https://api.digitalocean.com/images/%s%s" % ( self.id, path ), params=payload)
+        data = r.json()
+        self.call_response = data
+        if data['status'] != "OK":
+            raise Exception(data[u'error_message'])
+   
+        return data
+
+    def Destroy(self):
+        """
+            Destroy the image
+        """
+        self.__call_api("/destroy/")
+
+    def transfer(self, new_region_id):
+        """
+            Transfer the image
+        """
+        self.__call_api("/transfer/", {"region_id": new_region_id})

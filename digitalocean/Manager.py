e063ae1af58cb277b0d85c7a708909502f79afaf
Alex Tokar <alext@bitbamboo.com>
2013-09-11 16:06:46 -0700

    fixed key error bug

--- digitalocean/Manager.py
+++ digitalocean/Manager.py
@@ -20,7 +20,7 @@ class Manager(object):
         data = r.json()
         self.call_response = data
         if data['status'] != "OK":
-            raise Exception(data[u'error_message'])
+            raise Exception(data[u'message'])
         return data
 
     def get_all_regions(self):

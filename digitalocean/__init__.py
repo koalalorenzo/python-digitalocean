5b35122c91d50c5958c4e65622bc17412a657d3a
Lorenzo Setale <koalalorenzo@gmail.com>
2014-03-05 14:34:58 +0100

    Updating my personal website.

--- digitalocean/__init__.py
+++ digitalocean/__init__.py
@@ -2,7 +2,7 @@
 """digitalocean API to manage droplets"""
 
 __version__ = "0.7"
-__author__ = "Lorenzo Setale ( http://who.is.koalalorenzo.com/? )"
+__author__ = "Lorenzo Setale ( http://who.is.lorenzo.setale.me/? )"
 __author_email__ = "koalalorenzo@gmail.com"
 __license__ = "See: http://creativecommons.org/licenses/by-nd/3.0/ "
 __copyright__ = "Copyright (c) 2012, 2013, 2014 Lorenzo Setale"

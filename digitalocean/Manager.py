db84b30229ba4dc148725d025cea21c48825c511
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2014-08-02 18:13:53 -0400

    Port SSHKeys.

--- digitalocean/Manager.py
+++ digitalocean/Manager.py
@@ -197,14 +197,14 @@ class Manager(object):
         """
             This function returns a list of SSHKey object.
         """
-        data = self.__call_api("/ssh_keys/")
+        data = self.__call_api("/account/keys/")
         ssh_keys = list()
         for jsoned in data['ssh_keys']:
             ssh_key = SSHKey()
             ssh_key.id = jsoned['id']
             ssh_key.name = jsoned['name']
-            ssh_key.client_id = self.client_id
-            ssh_key.api_key = self.api_key
+            ssh_key.public_key = jsoned['public_key']
+            ssh_key.fingerprint = jsoned['fingerprint']
+            ssh_key.token = self.token
             ssh_keys.append(ssh_key)
         return ssh_keys
-

de3231d66cf99a372712159673ecf0e94880694a
Lorenzo Setale <koalalorenzo@gmail.com>
2014-08-11 18:21:57 +0200

    Created __call_api (basic example version, it will be overwritten)

--- digitalocean/baseapi.py
+++ digitalocean/baseapi.py
@@ -3,6 +3,7 @@ class BaseAPI(object):
         Basic api class for
     """
     self.token = ""
+    self.call_response = None
 
     def __init__(self, arg):
         super(BaseAPI, self).__init__()
@@ -22,7 +23,7 @@ class BaseAPI(object):
         headers['content-type'] = 'application/x-www-form-urlencoded'
         return requests.delete(url, headers=headers, params=params)
 
-    def __perform_request(self, url, params=dict()):
+    def __perform_request(self, url, type='GET', params=dict()):
         """
             This method will perform the real request,
             in this way we can customize only the "output" of the API call by
@@ -39,6 +40,15 @@ class BaseAPI(object):
         else:
             r = self.__perform_get(url, headers=headers, params=params)
         return r
+
+    def __call_api(self, path, action, type="GET" params=dict()):
+        """
+            Basic "call_api" method. It should not work.
+        """
+        url = "https://api.digitalocean.com/v2/%s/%s" % (path, action)
+        req = self.__perform_request(url, type)
+        self.call_response = req.json() # raise an error in case of problems.
+        return self.call_response
 
     def call_api(self, *args, **kargs):
         """

2b6ae6de2f3a122f75471a2e7381fbf53fc30867
Lorenzo Setale <koalalorenzo@gmail.com>
2014-08-11 23:02:44 +0200

    Using the new BaseAPI class (to be tested)

--- digitalocean/Domain.py
+++ digitalocean/Domain.py
@@ -1,49 +1,22 @@
 import requests
 from .Record import Record
+from .baseapi import BaseAPI
 
-class Domain(object):
+class Domain(BaseAPI):
+    name = None
+    ttl = None
+    zone_file = None
+    ip_address = None
+
     def __init__(self, *args, **kwargs):
-        self.token = ""
-        self.name = None
-        self.ttl = None
-        self.zone_file = None
-        ip_address = None
-
+        super(Domain, self).__init__()
         #Setting the attribute values
         for attr in kwargs.keys():
             setattr(self,attr,kwargs[attr])
 
-    def __call_api(self, type, path, params=dict()):
-        headers = {'Authorization':'Bearer ' + self.token}
-        if type == 'POST':
-            headers['content-type'] = 'application/json'
-            r = requests.post("https://api.digitalocean.com/v2/domains%s" %
-                              path,
-                              headers=headers,
-                              params=params)
-        elif type == 'DELETE':
-            headers['content-type'] = 'application/x-www-form-urlencoded'
-            r = requests.delete("https://api.digitalocean.com/v2/domains%s" %
-                              path,
-                              headers=headers,
-                              params=params)
-        else:
-            r = requests.get("https://api.digitalocean.com/v2/domains%s" %
-                              path,
-                              headers=headers,
-                              params=params)
-        # A successful delete returns "204 No Content"
-        if r.status_code != 204:
-            data = r.json()
-            self.call_response = data
-            if r.status_code not in [requests.codes.ok, 202, 201]:
-                msg = [data[m] for m in ("id", "message") if m in data][1]
-                raise Exception(msg)
-   
-            return data
-
     def load(self):
-        domain = self.__call_api("GET", "")['domain']
+        domains = self.get_data("https://api.digitalocean.com/v2/domains")
+        domain = domains['domain']
         self.live_zone_file = domain['zone_file']
         self.ttl = domain['ttl']
         self.name = domain['name']
@@ -52,7 +25,10 @@ class Domain(object):
         """
             Destroy the domain by name
         """
-        self.__call_api("DELETE", '/' + self.name)
+        domain = self.get_data(
+            "https://api.digitalocean.com/v2/domains/%s" % self.name,
+            type="DELETE"
+        )
 
     def create(self):
         """
@@ -62,14 +38,25 @@ class Domain(object):
                 "name": self.name,
                 "ip_address": self.ip_address,
             }
-        data = self.__call_api("POST", "", data)
+
+        domain = self.get_data(
+            "https://api.digitalocean.com/v2/domains",
+            type="POST",
+            params=data
+        )
+        return domain
 
     def get_records(self):
         """
             Returns a list of Record objects
         """
         records = []
-        data = self.__call_api("GET", '/' + self.name + "/records/")
+        data = self.get_data(
+            "https://api.digitalocean.com/v2/domains/%s/records/" % self.name,,
+            type="GET",
+            params=data
+        )
+
         for record_data in data['domain_records']:
             record = Record(domain_name=self.name,
                             id=record_data.pop('id'))

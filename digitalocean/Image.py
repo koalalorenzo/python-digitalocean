148756d435e59898b136007c257541dfa577fc38
Lorenzo Setale <koalalorenzo@gmail.com>
2014-08-12 00:03:17 +0200

    Removed call_api: not useful anymore

--- digitalocean/Image.py
+++ digitalocean/Image.py
@@ -15,38 +15,6 @@ class Image(BaseAPI):
         if token:
             self.token = token
 
-    def __call_api(self, type, path, params=dict()):
-        payload = {}
-        headers = {'Authorization':'Bearer ' + self.token}
-        payload.update(params)
-        if type == 'POST':
-            headers['content-type'] = 'application/json'
-            r = requests.post("https://api.digitalocean.com/v2/images/%s%s" %
-                             (self.id, path),
-                              headers=headers,
-                              params=payload)
-        elif type == 'PUT':
-            headers['content-type'] = 'application/json'
-            r = requests.put("https://api.digitalocean.com/v2/images/%s%s" %
-                             (self.id, path),
-                              headers=headers,
-                              params=payload)
-        elif type == 'DELETE':
-            headers['content-type'] = 'application/x-www-form-urlencoded'
-            r = requests.delete("https://api.digitalocean.com/v2/images/%s" %
-                             (self.id),
-                              headers=headers,
-                              params=payload)
-        # A successful delete returns "204 No Content"
-        if r.status_code != 204:
-            data = r.json()
-            self.call_response = data
-            if r.status_code != requests.codes.ok:
-                msg = [data[m] for m in ("id", "message") if m in data][1]
-                raise Exception(msg)
-
-            return data
-
     def destroy(self):
         """
             Destroy the image

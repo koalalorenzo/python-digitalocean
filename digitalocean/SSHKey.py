66d5af4c8dcd4a5e61ce419518ed853a51be1cd6
Lorenzo Setale <koalalorenzo@gmail.com>
2014-08-12 00:18:26 +0200

    Removed from SSHKey class the __call_api method: obsolete

--- digitalocean/SSHKey.py
+++ digitalocean/SSHKey.py
@@ -16,41 +16,6 @@ class SSHKey(BaseAPI):
         for attr in kwargs.keys():
             setattr(self,attr,kwargs[attr])
 
-    def __call_api(self, type, params=dict()):
-        headers = {'Authorization':'Bearer ' + self.token}
-        if type == 'POST':
-            headers['content-type'] = 'application/json'
-            r = requests.post("https://api.digitalocean.com/v2/account/keys/",
-                              headers=headers,
-                              params=params)
-        elif type == 'PUT':
-            headers['content-type'] = 'application/json'
-            r = requests.put("https://api.digitalocean.com/v2/account/keys/%s" %
-                              self.id,
-                              headers=headers,
-                              params=params)
-        elif type == 'DELETE':
-            headers['content-type'] = 'application/x-www-form-urlencoded'
-            r = requests.delete("https://api.digitalocean.com/v2/account/keys/%s" %
-                              self.id,
-                              headers=headers,
-                              params=params)
-        else:
-            r = requests.get("https://api.digitalocean.com/v2/account/keys/%s" %
-                             self.id,
-                             headers=headers,
-                             params=params)
-
-        # A successful delete returns "204 No Content"
-        if r.status_code != 204:
-            data = r.json()
-            self.call_response = data
-            if r.status_code not in [requests.codes.ok, 202, 201]:
-                msg = [data[m] for m in ("id", "message") if m in data][1]
-                raise Exception(msg)
-
-            return data
-
     def load(self):
         data = self.get_data(
             "account/keys/%s" % self.id,

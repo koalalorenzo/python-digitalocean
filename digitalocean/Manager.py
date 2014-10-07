26f21b5adef0afeb43d29ccc6c752d3e15a24e16
Lorenzo Setale <koalalorenzo@gmail.com>
2014-10-07 08:54:54 +0200

    Added a specific method that will return a specific Droplet by its ID.

--- digitalocean/Manager.py
+++ digitalocean/Manager.py
@@ -80,6 +80,12 @@ class Manager(BaseAPI):
             droplets.append(droplet)
         return droplets
 
+    def get_droplet(self, droplet_id):
+        """
+            Return a Droplet by its ID.
+        """
+        return Droplet.get_object(api_token=self.token, droplet_id=droplet_id)
+
     def get_all_sizes(self):
         """
             This function returns a list of Size object.

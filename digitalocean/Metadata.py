ad21106526ecfe694a5ffba600d4300813eb7352
Lorenzo Setale <koalalorenzo@gmail.com>
2014-10-19 22:57:55 +0200

    Implemented a custom get_data to get directly the request without authentication.

--- digitalocean/Metadata.py
+++ digitalocean/Metadata.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
+import requests
+try:
+    from urlparse import urljoin
+except:
+    from urllib.parse import urljoin
+
 from .baseapi import BaseAPI
 
 class Metadata(BaseAPI):
+    """
+        Metadata API: Provide useful information about the current Droplet.
+        See: https://developers.digitalocean.com/metadata/#introduction
+    """
     droplet_id = None
     end_point = "http://169.254.169.254/metadata/v1"
 
@@ -9,8 +19,19 @@ class Metadata(BaseAPI):
         super(Metadata, self).__init__(*args, **kwargs)
         self.end_point = "http://169.254.169.254/metadata/v1"
 
+    def get_data(self, url, headers=dict(), params=dict()):
+        """
+            Customized version of get_data to directly get the data without
+            using the authentication method.
+        """
+        if "https" not in url:
+            url = urljoin(self.end_point, url)
+
+        response = requests.get(url, headers=headers, params=params)
+        return response.content
+
     def load(self):
-        metadata = self.get_data(".json")
+        metadata = self.get_data("v1.json")
 
         for attr in metadata.keys():
             setattr(self,attr,metadata[attr])

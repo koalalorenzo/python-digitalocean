a82488489eb8b2a1864f6bb312bedad0816bc69b
Lorenzo Setale <lorenzo.setale@me.com>
2015-10-20 21:34:02 +0200

    New Version!

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if os.path.isfile("README.md"):
 
 setup(
     name='python-digitalocean',
-    version='1.6',
+    version='1.7',
     description='digitalocean.com API to manage Droplets and Images',
     author='Lorenzo Setale ( http://who.is.lorenzo.setale.me/? )',
     author_email='koalalorenzo@gmail.com',

1d90d26151f71f306b120e792239c9a98f87d148
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2017-10-01 22:06:48 -0400

    Add create method to Firewalls.

--- digitalocean/Firewall.py
+++ digitalocean/Firewall.py
@@ -172,6 +172,38 @@ class Firewall(BaseAPI):
 
         return self
 
+    def create(self, *args, **kwargs):
+        inbound = jsonpickle.encode(self.inbound_rules, unpicklable=False)
+        outbound = jsonpickle.encode(self.outbound_rules, unpicklable=False)
+        params = {'name': self.name,
+                  'droplet_ids': self.droplet_ids,
+                  'inbound_rules': jsonpickle.decode(inbound),
+                  'outbound_rules': jsonpickle.decode(outbound),
+                  'tags': self.tags}
+
+        data = self.get_data('firewalls/', type=POST, params=params)
+
+        if data:
+            self.id = data['firewall']['id']
+            self.name = data['firewall']['name']
+            self.status = data['firewall']['status']
+            self.created_at = data['firewall']['created_at']
+            self.pending_changes = data['firewall']['pending_changes']
+            self.droplet_ids = data['firewall']['droplet_ids']
+            self.tags = data['firewall']['tags']
+
+            in_rules = list()
+            for rule in data['firewall']['inbound_rules']:
+                in_rules.append(InboundRule(**rule))
+            self.inbound_rules = in_rules
+
+            out_rules = list()
+            for rule in data['firewall']['outbound_rules']:
+                out_rules.append(OutboundRule(**rule))
+            self.outbound_rules = out_rules
+
+        return self
+
     def add_droplets(self, droplet_ids):
         """
             Add droplets to this Firewall.

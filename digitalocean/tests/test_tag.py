c6fcfdb02f1cb1f378b205f18a68e309f032f3b8
Mioi Hanaoka <mhanaoka@slack-corp.com>
2018-10-11 10:50:14 -0700

    Remove trailing slash from request to retrieve tag
    This was fixed for the `get_all_tags()` function in PR #230, but was not
    fixed for the function to create new tags.
    
    creating new tags is resulting in a `NotFoundError` exception. This
    should fix it.

--- digitalocean/tests/test_tag.py
+++ digitalocean/tests/test_tag.py
@@ -34,7 +34,7 @@ class TestTags(BaseTest):
     def test_create(self):
         data = self.load_from_file('tags/single.json')
 
-        url = self.base_url + "tags/"
+        url = self.base_url + "tags"
         responses.add(responses.POST,
                       url,
                       body=data,

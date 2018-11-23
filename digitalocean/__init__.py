7b3cb253d8d423f14ca4d274b8da8dc4cf1bbb4c
wacuuu <jwalecki1996@gmail.com>
2018-11-23 22:25:36 +0100

    Changes in init to make documentation aligned with code
    Changing __init__.py so that imports and docs have the same path.
    For example:
    old way:
    import digitalocean
    digitalocean.ForwardingRule()
    new way:
    import digitalocean
    digitalocean.LoadBalancer.ForwardingRule()

--- digitalocean/__init__.py
+++ digitalocean/__init__.py
@@ -7,23 +7,22 @@ __author_email__ = "lorenzo@setale.me"
 __license__ = "LGPL v3"
 __copyright__ = "Copyright (c) 2012, 2013, 2014, 2015, 2016, 2017, 2018 Lorenzo Setale"
 
-from .Manager import Manager
-from .Droplet import Droplet, DropletError, BadKernelObject, BadSSHKeyFormat
-from .Region import Region
-from .Size import Size
-from .Image import Image
-from .Action import Action
-from .Account import Account
-from .Domain import Domain
-from .Record import Record
-from .SSHKey import SSHKey
-from .Kernel import Kernel
-from .FloatingIP import FloatingIP
-from .Volume import Volume
-from .baseapi import Error, TokenError, DataReadError
-from .Tag import Tag
-from .LoadBalancer import LoadBalancer
-from .LoadBalancer import StickySesions, ForwardingRule, HealthCheck
-from .Certificate import Certificate
-from .Snapshot import Snapshot
-from .Firewall import Firewall, InboundRule, OutboundRule, Destinations, Sources
+import Manager
+import Droplet
+import Region
+import Size
+import Image
+import Action
+import Account
+import Domain
+import Record
+import SSHKey
+import Kernel
+import FloatingIP
+import Volume
+import baseapi
+import Tag
+import LoadBalancer
+import Certificate
+import Snapshot
+import Firewall

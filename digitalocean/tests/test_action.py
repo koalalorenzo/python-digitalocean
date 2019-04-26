673665a7aabfed2753737d70169cf3485e06f48a
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2019-04-26 17:15:48 -0400

    Call load_directly() for Actions without a droplet.id

--- digitalocean/tests/test_action.py
+++ digitalocean/tests/test_action.py
@@ -30,5 +30,24 @@ class TestAction(BaseTest):
         self.assertEqual(self.action.id, 39388122)
         self.assertEqual(self.action.region_slug, 'nyc3')
 
+    @responses.activate
+    def test_load_without_droplet_id(self):
+        data = self.load_from_file('actions/ipv6_completed.json')
+
+        url = self.base_url + "actions/39388122"
+        responses.add(responses.GET,
+                      url,
+                      body=data,
+                      status=200,
+                      content_type='application/json')
+
+        self.action.load()
+
+        self.assert_get_url_equal(responses.calls[0].request.url, url)
+        self.assertEqual(self.action.status, "completed")
+        self.assertEqual(self.action.id, 39388122)
+        self.assertEqual(self.action.region_slug, 'nyc3')
+
+
 if __name__ == '__main__':
     unittest.main()
